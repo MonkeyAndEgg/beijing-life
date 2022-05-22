36dabc2e3fc515a7190df6776e5dc2c5913289db
MonkeyAndEgg <412793874@qq.com>
2022-05-21 17:01:07 -0700

    updated images for money events

--- src/constants/moneyEvent.ts
+++ src/constants/moneyEvent.ts
@@ -1,11 +1,11 @@
 import { MoneyEvent } from "../models/moneyEvent";
 
 export const moneyEvents: MoneyEvent[] = [
-  { event: '俺怜悯扮演成乞丐的老太太们。俺的现金减少了10%', img: '', percent: 0.1 },
-  { event: '一个汉子在街头拦住俺：“哥们，给点钱用!”，俺的现金减少了10%', img: '', percent: 0.1 },
-  { event: '一个大个子碰了俺一下，说：“别挤了!”，俺的现金减少了40%', img: '', percent: 0.4 },
-  { event: '三个带红袖章的老太太揪住俺：“你是外地人?罚款!”，俺的现金减少了20%', img: '', percent: 0.2 },
-  { event: '两个猛男揪住俺：“交市话费、长话附加费、上网费。”，俺的现金减少了15%', img: '', percent: 0.15 },
-  { event: '副主任严肃地说：“晚上别来我家给我送钱。”， 俺的现金减少了10%', img: '', percent: 0.1 },
-  { event: '北京空气污染得厉害,俺去氧吧吸氧..现金减少了5%', img: '', percent: 0.05 },
+  { event: '俺怜悯扮演成乞丐的老太太们。俺的现金减少了10%', img: '/images/sad.jpg', percent: 0.1 },
+  { event: '一个汉子在街头拦住俺：“哥们，给点钱用!”，俺的现金减少了10%', img: '/images/sad.jpg', percent: 0.1 },
+  { event: '一个大个子碰了俺一下，说：“别挤了!”，俺的现金减少了40%', img: '/images/sad.jpg', percent: 0.4 },
+  { event: '三个带红袖章的老太太揪住俺：“你是外地人?罚款!”，俺的现金减少了20%', img: '/images/sad.jpg', percent: 0.2 },
+  { event: '两个猛男揪住俺：“交市话费、长话附加费、上网费。”，俺的现金减少了15%', img: '/images/sad.jpg', percent: 0.15 },
+  { event: '副主任严肃地说：“晚上别来我家给我送钱。”， 俺的现金减少了10%', img: '/images/sad.jpg', percent: 0.1 },
+  { event: '北京空气污染得厉害,俺去氧吧吸氧..现金减少了5%', img: '/images/sad.jpg', percent: 0.05 },
 ];

ba76e8ea005e3c98fc40ee02dda7b6ca9c5cb6b1
MonkeyAndEgg <412793874@qq.com>
2022-05-26 19:56:38 -0700

    added asset reputation constants

---
+++ src/constants/AssetRep.ts
@@ -0,0 +1,10 @@
+export enum AssetsReputation {
+  UltraPoor = '债台高筑',
+  Poor = '一贫如洗',
+  Zero = '屁都没有',
+  LessThanTenW = '有点小钱',
+  LessThanFiftyW = '小康',
+  LessThanHundredW = '中产阶级',
+  LessThanFiveHundredW = '脱离韭菜',
+  UltraRich = '人间已是巅',
+}

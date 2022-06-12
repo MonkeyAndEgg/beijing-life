8dbc3bd849f6c82177d5ae1968646c4fff7e4f76
MonkeyAndEgg <412793874@qq.com>
2022-06-12 00:28:02 -0700

    added prefix for favicon path

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,14 @@ import { AppProps } from "next/app";
 import Head from "next/head";
 import { Provider } from "react-redux";
 import { store, wrapper } from "../redux/store";
+import { BASE_PATH } from "../src/constants/app";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider>
       <Provider store={store}>
         <Head>
-          <link rel="shortcut icon" href="/beijing-life/favicon.ico" />
+          <link rel="shortcut icon" href={`${BASE_PATH}/favicon.ico`} />
         </Head>
         <Component {...pageProps}></Component>
       </Provider>
